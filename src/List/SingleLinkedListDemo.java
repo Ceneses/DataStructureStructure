76c4b3c050227efeec0a6c41ef8c2eab0e06d706
Ceneses <wo51study@163.com>
2020-04-29 09:34:38 +0800

    add function addByOrder()

--- src/List/SingleLinkedListDemo.java
+++ src/List/SingleLinkedListDemo.java
@@ -7,6 +7,13 @@ public class SingleLinkedListDemo {
         Node node3 = new Node(14,null);
         Node node4 = new Node(15,null);
         SingleList singleList = new SingleList();
+//        singleList.addByOrder(node1);
+//        singleList.addByOrder(node2);
+//        singleList.addByOrder(node2);
+//        singleList.addByOrder(node4);
+//        singleList.addByOrder(node3);
+//        singleList.list();
+
         singleList.add(node1);
         singleList.add(node2);
         singleList.add(node3);
@@ -29,12 +36,37 @@ class SingleList{
         temp.next = node;
     }
 
-    public void list(){
+    public void addByOrder(Node node){
         Node temp = head;
+        boolean flag = false; //代表着插入元素在单链表中是否存在相同的元素
+        // 找到插入点
         while(temp.next != null){
+            if(temp.next.data > node.data){
+                break;
+            }else if(temp.next.data == node.data){
+                flag = true;
+                break;
+            }else{
+                temp = temp.next;
+            }
+        }
+        // 插入数值
+        if(flag){
+            System.out.println(node+"元素已经存在，无法插入");
+        }else{
+            node.next = temp.next;
+            temp.next = node;
+        }
+    }
+
+    public void list(){
+        Node temp = head; // 代表着当前节点
+        while(temp.next != null){ // 最后的一个节点是没有进入while循环体
             System.out.println(temp);
             temp = temp.next;
         }
+        // 最后的一个节点也要记得输出和打印
+        System.out.println(temp);
     }
 }
 
