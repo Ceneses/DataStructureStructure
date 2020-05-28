f8861118a08afaf9b68014dd3d7f336c08613246
Ceneses <wo51study@163.com>
2020-05-28 23:56:59 +0800

    生成无限流

--- src/Stream/TestStream.java
+++ src/Stream/TestStream.java
@@ -31,5 +31,8 @@ public class TestStream {
         // 迭代
         Stream<Integer> stream4 = Stream.iterate(0, (x) -> x + 2);
         stream4.limit(10).forEach(System.out::println);
+
+        // 生成
+        Stream.generate(() -> Math.random()).limit(5).forEach(System.out::println);
     }
 }
